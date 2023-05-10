99145a09d0776c39f14bd26f0fc15c969407bd8b
coding-geek07 <exploringtechiegeek@gmail.com>
2023-05-10 16:38:51 +0100

    DSA: Recursion: string reverse solution

--- programs/Recursion/program.js
+++ programs/Recursion/program.js
@@ -1,16 +1,5 @@
-function fib(num) {
-    if (num === 1 || num === 2) return 1;
-    let arr = [1, 1];
-    let i = 0, j = 1;
-    function helperMethod(num) {
-        if (arr.length < num) {
-            arr.push(arr[i] + arr[j]);
-            i++;
-            j++;
-            return helperMethod(num);
-        }
-        return arr[j];
-    }
-    return helperMethod(num);
+function reverse(str) {
+    if (!str.length) return '';
+    return str[str.length - 1] + reverse(str.slice(0, str.length - 1));
 }
-console.log(fib(35));
+console.log(reverse('awesome'));

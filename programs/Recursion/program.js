fa1313e83c6c80fdb33a7357dd50d048bf490a26
coding-geek07 <exploringtechiegeek@gmail.com>
2023-05-10 15:32:53 +0100

    DSA: Recursion: base exponent solution

--- programs/Recursion/program.js
+++ programs/Recursion/program.js
@@ -1,36 +1,10 @@
-/* 
-Write a factorial recursion
-input : 4
-output : 4 * 3 * 2* 1 : 24
+function power(base, exponent) {
+    if (exponent === 0) return 1;
 
-function getFactorialOf(num) {
-    if (num === 1) return 1;
-    return num * getFactorialOf(num - 1);
+    return base * (power(base, exponent - 1));
 }
-
-console.log(getFactorialOf(5));
-*/
-/* 
-    collect odd values from array
-*/
-
-function collectOddValues(arr) {
-    let newArr = [];
-
-    if (arr.length === 0) {
-        return newArr;
-    }
-
-    if (arr[0] % 2 !== 0) {
-        newArr.push(arr[0]);
-    }
-
-    newArr = newArr.concat(collectOddValues(arr.slice(1)));
-    return newArr;
-}
-
-collectOddValues([1, 2, 3, 4, 5])
-
-
-
-
+console.log(power(2, 0));
+/* 2 * power(2, 3)
+    2 * power(2, 2)
+        2 * power(2, 1)
+            2 * power(2, 0) */
